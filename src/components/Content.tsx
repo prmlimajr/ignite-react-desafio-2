3ff2449250a159fe8efb6b565d1adbb6cc3e829d
Paulo Lima <prmlimajr@hotmail.com>
2021-03-20 19:27:39 -0300

    Componentização

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,48 @@
-export function Content() {
-  // Complete aqui
+import React, { useState } from 'react'
+import { MovieCard } from './MovieCard';
+
+
+import '../styles/content.scss';
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface ContentProps {
+  selectedGenre: GenreResponseProps
+  movies: MovieProps[]
+}
+
+export function Content ({ movies, selectedGenre } : ContentProps) {
+  
+  return (
+    <div>
+      <div className="container">
+        <header>
+          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {movies.map(movie => (
+              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+      </div>
+    </div>
+  )
 }
